5f8c3a7e0bbd057bbf76c8fa31f164262db7869e
Jan Betley <jan.betley@gmail.com>
2023-05-28 20:09:48 +0100

    wip

--- t1.py
+++ t1.py
@@ -1,11 +1,16 @@
 # %%
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 import torch as t
 from torch import nn
 import random
 
 MAIN = '__main__'
 N_FREQ = 64
+
+device = 'cpu'
 
 # %%
 # Implementation of (a + b) % p according to section 3.1 of https://arxiv.org/pdf/2301.05217.pdf
@@ -47,9 +52,162 @@ def sum_mod_p(a: int, b: int, p: int, size: int = 64) -> int:
     logits = freq_2_logit(sin_a, cos_a, sin_b, cos_b, freqs, p)
     return logits.argmax()
  
+# if MAIN:
+#    for a, b, p in t.randint(1, 1000, (100, 3)):
+#         a, b, p = int(a), int(b), int(p)
+#         assert sum_mod_p(a, b, p, size=15).item() == (a + b) % p, f"NOPE: {a} {b} {p}"
+
+# %%
+class SingleLayer(nn.Module):
+    def __init__(self, p, n_freqs=50):
+        super().__init__()
+        self.p = p
+        self.n_freqs = n_freqs
+
+        freqs = (2  * t.pi / p) * t.randint(1, 10 ** 5, (n_freqs,))
+        self.freqs = freqs.to(float)
+
+        self.sin = nn.Linear(n_freqs, n_freqs, bias=False).double()
+        self.cos = nn.Linear(n_freqs, n_freqs, bias=False).double()
+
+    def forward(self, x):
+        print(x.device)
+        a = x[:, 0]
+        b = x[:, 1]
+
+        print(x.shape)
+        print(a.shape)
+        print(b.shape)
+        sin_a = self.sin(a * self.freqs)
+        cos_a = self.cos(a * self.freqs)
+
+        sin_b = self.sin(b * self.freqs)
+        cos_b = self.cos(b * self.freqs)
+        
+        print(sin_a.shape)
+
+        return freq_2_logit(sin_a, cos_a, sin_b, cos_b, self.freqs, self.p)
+
+# %%
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import CSVLogger
+
+# %%
+
+class ModDataset(t.utils.data.Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, p: int, nth: int):
+        self.p = p
+        self.nth = nth
+
+        self.data = self.get_data()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+  
+    def get_data(self):
+        all_data = []
+        for i in range(self.p):
+            for j in range(self.p):
+                input = t.tensor([i, j], dtype=float)
+                label = (i + j) % self.p
+                all_data.append((input, label))
+    
+        return [x for i, x in enumerate(all_data) if not i % self.nth]
+
+# %%
+
+def get_datasets(p):
+    trainset = ModDataset(p, 7)
+    testset = ModDataset(p, 3)
+
+    return trainset, testset
+
+# %%
+class LitSingleLayer(pl.LightningModule):
+    def __init__(self, p: int, batch_size: int, max_epochs: int):
+        super().__init__()
+        self.p = p
+        self.model = SingleLayer(p)
+        self.batch_size = batch_size
+        self.max_epochs = max_epochs
+        self.trainset, self.testset = get_datasets(p)
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx: int) -> t.Tensor:
+        in_, labels = batch
+        in_ = in_.to(device)
+        labels = labels.to(device)
+        logits = self(in_)
+        print(len(logits))
+        print(len(labels))
+        loss = t.nn.functional.cross_entropy(logits, labels)
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        '''
+        Choose what optimizers and learning-rate schedulers to use in your optimization.
+        '''
+        return t.optim.Adam(self.parameters())
+    
+    def train_dataloader(self):
+        '''
+        Return the training dataloader.
+        '''
+        return t.utils.data.DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True)
+
+# %%
+
+# Create the model & training system
+
 if MAIN:
-    for a, b, p in t.randint(1, 1000, (100, 3)):
-        a, b, p = int(a), int(b), int(p)
-        assert sum_mod_p(a, b, p, size=64).item() == (a + b) % p, f"NOPE: {a} {b} {p}"
+    p = 113
+    batch_size = 64
+    max_epochs = 3
+    model = LitSingleLayer(p, batch_size, max_epochs).to(device)
+    assert str(model.device) == device, f"model has device {model.device}"
+    
+    # Get a logger, to record metrics during training
+    logger = CSVLogger(save_dir=os.getcwd() + "/logs", name="t1")
+    
+    # Train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+    trainer = pl.Trainer(
+        max_epochs=max_epochs,
+        logger=logger,
+        log_every_n_steps=1,
+    )
+    trainer.fit(model=model)
+
+# %%
+import pandas as pd
+
+if MAIN:
+    metrics = pd.read_csv(f"{trainer.logger.log_dir}/metrics.csv")
+    
+    metrics.head()
+
+# %%
+
+
+if MAIN:
+    print(metrics["train_loss"].values)
+    from plotly_utils import line
+    line(
+        metrics["train_loss"].values,
+        x=metrics["step"].values,
+        yaxis_range=[0, metrics["train_loss"].max() + 0.1],
+        labels={"x": "Batches seen", "y": "Cross entropy loss"},
+        title="ConvNet training on MNIST",
+        width=800,
+        hovermode="x unified",
+        template="ggplot2", # alternative aesthetic for your plots (-:
+    )
 
 # %%

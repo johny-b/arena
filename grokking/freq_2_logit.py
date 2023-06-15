808fc2a64bca3e7df612a1480285e7d483d02642
Jan Betley <jan.betley@gmail.com>
2023-06-15 12:22:58 +0100

    Some initial work on reproducing grokking in another setting
    Only (somewhat) valuable thing is grokking/freq_2_logit.py -
    implementation of the grokking modular addition algorithm.

---
+++ grokking/freq_2_logit.py
@@ -0,0 +1,108 @@
+# %%
+import torch as t
+
+MAIN = __name__ == '__main__'
+# %%
+# Implementation of (a + b) % p according to section 3.1 of https://arxiv.org/pdf/2301.05217.pdf
+def freq_2_logit(
+    sin_a: t.Tensor, 
+    cos_a: t.Tensor, 
+    sin_b: t.Tensor, 
+    cos_b: t.Tensor,
+    freqs: t.Tensor, 
+    p: int,
+):
+    sin_sum = sin_a * cos_b + cos_a * sin_b
+    cos_sum = cos_a * cos_b - sin_a * sin_b
+
+    c = t.arange(p, dtype=float)
+    sin_c = t.sin(c)
+    cos_c = t.cos(c)
+
+    x = c.unsqueeze(0)
+    freqs = freqs.unsqueeze(0)
+    cos_c = t.cos(x.T @ freqs)
+    sin_c = t.sin(x.T @ freqs)
+
+    cos_a_plus_b_minus_c = cos_sum * cos_c + sin_sum * sin_c
+
+    return cos_a_plus_b_minus_c.sum(dim=-1)
+
+# %%
+# Test freq_2_logit
+def sum_mod_p(a: int, b: int, p: int, size: int = 64) -> int:
+    """Returns (a + b) % p using freq_2_logit"""
+    freqs = (2  * t.pi / p) * t.randint(1, 10 ** 5, (size,))
+    freqs = freqs.to(float)
+    
+    sin_a = t.sin(a * freqs)
+    cos_a = t.cos(a * freqs)
+
+    sin_b = t.sin(b * freqs)
+    cos_b = t.cos(b * freqs)
+
+    logits = freq_2_logit(sin_a, cos_a, sin_b, cos_b, freqs, p)
+    return logits.argmax()
+ 
+if MAIN:
+   for a, b, p in t.randint(1, 1000, (100, 3)):
+        a, b, p = int(a), int(b), int(p)
+        assert sum_mod_p(a, b, p, size=64).item() == (a + b) % p, f"NOPE: {a} {b} {p}"
+
+# %%
+
+def batched_freq_2_logit(
+    sin_a: t.Tensor,
+    cos_a: t.Tensor,
+    sin_b: t.Tensor,
+    cos_b: t.Tensor,
+    freqs: t.Tensor,
+    p: int,
+):
+    sin_sum = sin_a * cos_b + cos_a * sin_b  # batch, freqs
+    cos_sum = cos_a * cos_b - sin_a * sin_b  # batch, freqs
+
+    c = t.arange(p, dtype=float).to(sin_a.device)  # p
+    sin_c = t.sin(c)  # p
+    cos_c = t.cos(c)  # p
+
+    x = c.unsqueeze(0)  # 1 p
+    freqs = freqs.unsqueeze(0)  # 1 freqs
+    cos_c = t.cos(x.T @ freqs)  # p freqs
+    sin_c = t.sin(x.T @ freqs)  # p freqs
+
+    cos_sum = cos_sum.unsqueeze(1)  # batch 1 freqs
+    sin_sum = sin_sum.unsqueeze(1)  # batch 1 freqs
+
+    cos_c = cos_c.unsqueeze(0)  # 1 p freqs
+    sin_c = sin_c.unsqueeze(0)  # 1 p freqs
+
+    cos_a_plus_b_minus_c = cos_sum * cos_c + sin_sum * sin_c  # batch, p, freqs
+
+    return cos_a_plus_b_minus_c.sum(dim=-1)  # batch, p
+
+
+def batched_sum_mod_p(a: t.Tensor, b: t.Tensor, p: int, size: int = 64):
+    """Returns (a + b) % p using freq_2_logit"""
+    freqs = (2  * t.pi / p) * t.randint(1, 10 ** 5, (size,))
+    freqs = freqs.to(float)
+    
+    sin_a = t.sin(a.unsqueeze(0).T * freqs)
+    cos_a = t.cos(a.unsqueeze(0).T * freqs)
+    sin_b = t.sin(b.unsqueeze(0).T * freqs)
+    cos_b = t.cos(b.unsqueeze(0).T * freqs)
+
+    logits = batched_freq_2_logit(sin_a, cos_a, sin_b, cos_b, freqs, p)
+    return logits.argmax(-1)
+
+if MAIN:
+    p = t.randint(1, 1000, (1,)).item()
+    a = t.randint(1, p, (100,))
+    b = t.randint(1, p, (100,))
+    mod = (a + b) % p
+    out = batched_sum_mod_p(a, b, p, 64)
+    
+    t.testing.assert_close(out, mod)
+
+
+# %%
---
+++ grokking/t1.py
@@ -0,0 +1,130 @@
+# %%
+
+import pandas as pd
+import torch as t
+
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import CSVLogger
+
+import sys
+sys.path.append('/home/janbet/ARENA_2.0/chapter0_fundamentals/exercises')
+
+from plotly_utils import line, plot_train_loss_and_test_accuracy_from_metrics
+
+MAIN = __name__ == '__main__' 
+
+import models as m
+
+from IPython import get_ipython
+ipython = get_ipython()
+ipython.run_line_magic("load_ext", "autoreload")
+ipython.run_line_magic("autoreload", "2")
+
+
+device = t.device('cuda' if t.cuda.is_available() else 'cpu')
+
+# %%
+
+class ModDataset(t.utils.data.Dataset):
+    def __init__(self, p: int, nth: int):
+        self.p = p
+        self.nth = nth
+        self.data = self.get_data()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+  
+    def get_data(self):
+        all_data = []
+        for i in range(self.p):
+            for j in range(self.p):
+                input = t.tensor([i, j], dtype=float)
+                label = (i + j) % self.p
+                all_data.append((input, label))
+    
+        return [x for i, x in enumerate(all_data) if not i % self.nth]
+
+def get_datasets(p):
+    trainset = ModDataset(p, 7)
+    testset = ModDataset(p, 3)
+
+    return trainset, testset
+
+# %%
+class LitModel(pl.LightningModule):
+    def __init__(self, model, batch_size: int, max_epochs: int):
+        super().__init__()
+        self.model = model
+        self.batch_size = batch_size
+        self.max_epochs = max_epochs
+        self.trainset, self.testset = get_datasets(model.p)
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx: int) -> t.Tensor:
+        logits, labels = self._shared_train_val_step(batch)
+        loss = t.nn.functional.cross_entropy(logits, labels)
+        self.log("train_loss", loss)
+        return loss
+
+    def _shared_train_val_step(self, batch):
+        in_, labels = batch
+        in_ = in_.to(device)
+        labels = labels.to(device)
+        logits = self(in_)
+        return logits, labels
+    
+    def validation_step(self, batch, batch_idx: int) -> None:
+        logits, labels = self._shared_train_val_step(batch)
+        classifications = logits.argmax(dim=1)
+        accuracy = t.sum(classifications == labels) / len(classifications)
+        self.log("accuracy", accuracy)
+
+    def configure_optimizers(self):
+        '''
+        Choose what optimizers and learning-rate schedulers to use in your optimization.
+        '''
+        return t.optim.Adam(self.parameters())
+    
+    def train_dataloader(self):
+        '''
+        Return the training dataloader.
+        '''
+        return t.utils.data.DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return t.utils.data.DataLoader(self.testset, batch_size=self.batch_size, shuffle=True)
+
+# %%
+if MAIN:
+    p = 113
+    batch_size = 64
+    max_epochs = 100
+    n_freqs = 27
+
+    x = m.FreqsSinParam(p, n_freqs=n_freqs, sin_width=96)
+
+    model = LitModel(x, batch_size, max_epochs).to(device)
+    assert str(model.device).startswith(str(device)), f"model has device {model.device}"
+    
+    # Get a logger, to record metrics during training
+    logger = CSVLogger(save_dir=os.getcwd() + "/logs", name="t1")
+    
+    # Train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+    trainer = pl.Trainer(
+        max_epochs=max_epochs,
+        logger=logger,
+        log_every_n_steps=1,
+    )
+    trainer.fit(model=model)
+    # print(sorted(model.model.freqs.tolist()))
+
+if MAIN:
+    metrics = pd.read_csv(f"{trainer.logger.log_dir}/metrics.csv")    
+    plot_train_loss_and_test_accuracy_from_metrics(metrics, "Training ConvNet on MNIST data")
+    
+# %%

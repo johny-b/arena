0e44e4385b1d5d0e9edebfa631db8a9deff6f9b8
Jan Betley <jan.betley@gmail.com>
2023-06-16 16:01:33 +0100

    Rollout. Enough for sample_usage.

--- sample_usage.py
+++ sample_usage.py
@@ -4,7 +4,7 @@ import torch as t
 
 from procgen_tools.imports import load_model
 from procgen_tools import maze, visualization
-from procgen_tools.models import human_readable_action
+from procgen_tools.models import human_readable_action, human_readable_actions, MAZE_ACTION_DELTAS
 
 device = t.device("cpu")
 
@@ -45,6 +45,27 @@ with t.no_grad():
     categorical, value = policy(batched_obs)
     
 print(human_readable_action(categorical.logits.argmax()))
+print(human_readable_actions(categorical.logits))
 print(value)
 
 # %%
+# Perform a rollout (doesn't stop on cheese)
+max_steps = 10
+
+for i in range(max_steps):
+
+    state = maze.state_from_venv(venv)
+    current_pos = state.mouse_pos
+    
+    batched_obs = t.tensor(venv.reset(), dtype=t.float32, device=device)
+    with t.no_grad():
+        categorical, _ = policy(batched_obs)
+    
+    action = human_readable_action(categorical.logits.argmax())
+    step = MAZE_ACTION_DELTAS[action]
+    new_pos = (current_pos[0] + step[0], current_pos[1] + step[1])
+    state.set_mouse_pos(*new_pos)
+    venv = maze.venv_from_grid(state.inner_grid())
+    print(f"MOVE FROM {current_pos} to {new_pos}")
+
+    visualization.visualize_venv(venv, render_padding=False)
